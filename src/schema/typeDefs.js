dc564fb9585aa05bb025ead71658735d6481572c
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2026-03-01 18:20:19 +0100

    Add Movie queries and mutations to GraphQL schema

--- src/schema/typeDefs.js
+++ src/schema/typeDefs.js
@@ -1,11 +1,18 @@
 const typeDefs = `
   type Query {
     hello: String
+    movies(limit: Int, offset: Int): [Movie!]!
+    movie(id: Int!): Movie
+    searchMovies(title: String!): [Movie!]!
   }
 
   type Mutation {
     registerUser(email: String!, password: String!, name: String): AuthPayload!
     loginUser(email: String!, password: String!): AuthPayload!
+    
+    addMovie(title: String!, director: String, releaseYear: Int, genres: String, rating: Float, description: String): Movie!
+    updateMovie(id: Int!, title: String, director: String, releaseYear: Int, genres: String, rating: Float, description: String): Movie
+    deleteMovie(id: Int!): Boolean!
   }
 
   type AuthPayload {
@@ -17,6 +24,17 @@ const typeDefs = `
     id: Int!
     email: String!
     name: String
+    createdAt: String!
+  }
+
+  type Movie {
+    id: Int!
+    title: String!
+    director: String
+    releaseYear: Int
+    genres: String
+    rating: Float
+    description: String
     createdAt: String!
   }
 `;
