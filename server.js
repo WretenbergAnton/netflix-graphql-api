d81dc7a2233569735e360dde8fd57918c2b09be0
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2026-03-11 15:41:51 +0100

    Add Apollo Sandbox UI

--- server.js
+++ server.js
@@ -37,16 +37,48 @@ async function startServer() {
 
   app.use(express.json());
 
-  app.use(
-    '/graphql',
-    expressMiddleware(server, {
-      context: async ({ req }) => {
-        return {
-          authorization: req.headers.authorization || null,
-        };
-      },
-    })
-  );
+app.use(
+  '/graphql',
+  expressMiddleware(server, {
+    context: async ({ req }) => {
+      return {
+        authorization: req.headers.authorization || null,
+      };
+    },
+  })
+);
+
+app.get('/graphql', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset=utf-8/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Apollo Sandbox</title>
+        <style>
+          body { margin: 0; overflow: hidden; }
+          #sandbox { height: 100vh; width: 100%; }
+        </style>
+      </head>
+      <body>
+        <div id="sandbox"></div>
+        <script src="https://embeddable-sandbox.cdn.apollographql.com/_latest/embeddable-sandbox.umd.production.min.js"></script>
+        <script>
+          new window.EmbeddedSandbox({
+            target: "#sandbox",
+            initialState: {
+              document: "{ __typename }",
+              variables: {},
+              headers: {},
+              url: "https://netflix-graphql-api-production.up.railway.app/graphql",
+            },
+          });
+        </script>
+      </body>
+    </html>
+  `);
+});
 
   const PORT = process.env.PORT || 4000;
   
